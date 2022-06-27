3e5a6e64058d8bd9082e4a58b07dcbb2ba739e24
Pedro Arias <pedroariasperez96@gmail.com>
2022-06-27 17:30:19 +0200

    Added groot logger launch argument

--- launch/test.launch.py
+++ launch/test.launch.py
@@ -8,16 +8,18 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     tree = PathJoinSubstitution([
         FindPackageShare('behaviour_trees'),
-        'resource', 'beacon.xml'
+        'resource', 'takeoff.xml'
     ])
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
         DeclareLaunchArgument('tree', default_value=tree),
+        DeclareLaunchArgument('groot_logger', default_value='false'),
         Node(
             package="behaviour_trees",
             executable="behaviour_trees_main",
             namespace=LaunchConfiguration('drone_id'),
-            parameters=[{'tree': LaunchConfiguration('tree')}],
+            parameters=[{'tree': LaunchConfiguration('tree'),
+                         'use_groot': LaunchConfiguration('groot_logger')}],
             output="screen",
             emulate_tty=True,
             # arguments=['--ros-args', '--log-level', 'DEBUG']
