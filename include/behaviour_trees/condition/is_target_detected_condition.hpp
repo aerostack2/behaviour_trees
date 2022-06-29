21cc4e236c1b4012a6ebb726363655aea5a3efee
Pedro Arias <pedroariasperez96@gmail.com>
2022-06-29 10:42:42 +0200

    output port was returning an empty string fix

--- include/behaviour_trees/condition/is_target_detected_condition.hpp
+++ include/behaviour_trees/condition/is_target_detected_condition.hpp
@@ -60,7 +60,7 @@ namespace as2_behaviour_tree
         {
             return {BT::InputPort<std::string>("topic_name"), 
                     BT::InputPort<double>("dist_threshold"),
-                    BT::OutputPort("pose")};
+                    BT::OutputPort<std::string>("pose")};
         }
 
     private:
@@ -73,11 +73,14 @@ namespace as2_behaviour_tree
             RCLCPP_DEBUG(this->node_->get_logger(), "%f", dist);
             if (dist < threshold_)
             {
-                geometry_msgs::msg::PoseStamped rel_pose;
-                rel_pose.pose.position.x = msg->pose.position.x - this->current_pose_x_;
-                rel_pose.pose.position.y = msg->pose.position.y - this->current_pose_y_;
-                rel_pose.pose.position.z = msg->pose.position.z - this->current_pose_z_;
-                setOutput("pose", rel_pose);
+                geometry_msgs::msg::Pose rel_pose;
+                rel_pose.position.x = msg->pose.position.x - this->current_pose_x_;
+                rel_pose.position.y = msg->pose.position.y - this->current_pose_y_;
+                rel_pose.position.z = msg->pose.position.z - this->current_pose_z_;
+                std::string output = std::to_string(rel_pose.position.x) + ";" 
+                                    + std::to_string(rel_pose.position.y) + ";"
+                                    + std::to_string(rel_pose.position.z);
+                setOutput("pose", output);
                 is_target_ = true;
             }
         }
