f8ea64fd3a2a86befc5b82ac17dec08920429b7a
Pedro Arias <pedroariasperez96@gmail.com>
2022-07-22 10:59:36 +0200

    added dependencies installer

---
+++ install_bt_dependencies.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install ros-$ROS_DISTRO-behaviortree-cpp-v3
+sudo apt-get install ros-$ROS_DISTRO-nav2-msgs
+sudo apt-get install ros-$ROS_DISTRO-nav2-behavior-tree
