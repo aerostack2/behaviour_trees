6358f1791650fa8279153a9898732d9cdbfe3f12
Pedro Arias <pedroariasperez96@gmail.com>
2022-08-14 09:25:25 +0200

    added pick and unpick (wip)

---
+++ include/behaviour_trees/action/pick.hpp
@@ -0,0 +1,123 @@
+/*!*******************************************************************************************
+ *  \file       pick.hpp
+ *  \brief      Behaviour tree node to pick an object
+ *  \authors    Pedro Arias Pérez
+ *              Miguel Fernández Cortizas
+ *              David Pérez Saura
+ *              Rafael Pérez Seguí
+ *
+ *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
+ *              All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
+#ifndef PICK_HPP
+#define PICK_HPP
+
+#include <string>   
+
+#include "behaviortree_cpp_v3/action_node.h"
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "as2_core/names/topics.hpp"
+
+namespace as2_behaviour_tree
+{
+    class Pick : public BT::SyncActionNode
+    {
+    public:
+        Pick(const std::string &xml_tag_name, const BT::NodeConfiguration &conf)
+            : BT::SyncActionNode(xml_tag_name, conf)
+        {
+            node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+            callback_group_ = node_->create_callback_group(
+                rclcpp::CallbackGroupType::MutuallyExclusive,
+                false);
+            callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
+
+            getInput("object_pose_topic_name", object_pose_topic_name_);
+            getInput("info_topic_name", info_topic_name_);
+
+            rclcpp::SubscriptionOptions sub_option;
+            sub_option.callback_group = callback_group_;
+            object_pose_sub_ = node_->create_subscription<geometry_msgs::msg::PoseStamped>(
+                object_pose_topic_name_, rclcpp::QoS(10),
+                std::bind(&Pick::objectCallback, this, std::placeholders::_1),
+                sub_option);
+
+            info_sub_ = node_->create_subscription<geometry_msgs::msg::PoseStamped>(
+                info_topic_name_, rclcpp::QoS(10),
+                std::bind(&Pick::infoCallback, this, std::placeholders::_1),
+                sub_option);
+
+        }
+
+        Pick() = delete;
+
+        BT::NodeStatus tick()
+        {
+            callback_group_executor_.spin_some();
+            // path_pub_->publish();
+            return BT::NodeStatus::SUCCESS;
+
+        }
+
+        static BT::PortsList providedPorts()
+        {
+            return {
+                BT::InputPort<std::string>("object_pose_topic_name"),
+                BT::InputPort<std::string>("info_topic_name")
+            };
+        }
+
+    private:
+        void objectCallback(geometry_msgs::msg::PoseStamped::SharedPtr msg)
+        {
+            this->object_pose_ = *(msg);
+        }
+
+        void infoCallback(geometry_msgs::msg::PoseStamped::SharedPtr msg)
+        {
+            ;
+        }
+
+    private:
+        rclcpp::Node::SharedPtr node_;
+        rclcpp::CallbackGroup::SharedPtr callback_group_;
+        rclcpp::executors::SingleThreadedExecutor callback_group_executor_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr object_pose_sub_;
+        rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr info_sub_;
+
+        std::string object_pose_topic_name_;
+        std::string info_topic_name_;
+
+        geometry_msgs::msg::PoseStamped object_pose_;
+    };
+
+} // namespace as2_behaviour_tree
+
+#endif // PICK_HPP
--- src/behaviour_trees_node.cpp
+++ src/behaviour_trees_node.cpp
@@ -47,6 +47,8 @@
 #include "behaviour_trees/action/follow_path.hpp"
 #include "behaviour_trees/action/laser_planner.hpp"
 #include "behaviour_trees/action/area_planner.hpp"
+#include "behaviour_trees/action/pick.hpp"
+#include "behaviour_trees/action/unpick.hpp"
 #include "behaviour_trees/condition/is_flying_condition.hpp"
 #include "behaviour_trees/decorator/wait_for_event.hpp"
 #include "behaviour_trees/action/send_event.hpp"
@@ -84,7 +86,9 @@ int main(int argc, char *argv[])
     factory.registerNodeType<as2_behaviour_tree::FollowPath>("FollowPath");
     factory.registerNodeType<as2_behaviour_tree::LandAction>("Land");
     factory.registerNodeType<as2_behaviour_tree::LandAction>("LaserPlanner");
-    factory.registerNodeType<as2_behaviour_tree::LandAction>("AreaPlanner");
+    factory.registerNodeType<as2_behaviour_tree::AreaPlanner>("AreaPlanner");
+    factory.registerNodeType<as2_behaviour_tree::Pick>("Pick");
+    factory.registerNodeType<as2_behaviour_tree::Unpick>("Unpick");
     factory.registerNodeType<as2_behaviour_tree::IsFlyingCondition>("IsFlying");
     factory.registerNodeType<as2_behaviour_tree::WaitForEvent>("WaitForEvent");
     factory.registerNodeType<as2_behaviour_tree::SendEvent>("SendEvent");
